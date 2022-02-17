498f23f40d1f2258231b7d4a7adaa33afbca6d63
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-02-17 23:39:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {    
+int gcd(int a, int b) {
     if (a % b == 0)
-        return b;    
+        return b;
     if (b % a == 0)
-        return a;    
+        return a; 
     if (a > b)
         return(a % b, b);
     return(a, b % a);
